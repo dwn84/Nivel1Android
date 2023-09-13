7ff2d9bbdcb4bc2fb7cfa4713ffa60a764b400f1
dwn84 <50523550+dwn84@users.noreply.github.com>
2023-09-13 08:36:12 -0500

    Update menuppal.java

--- manejoClases/src/manejoClases/menuppal.java
+++ manejoClases/src/manejoClases/menuppal.java
@@ -1,17 +1,23 @@
 package manejoClases;
+import javax.swing.JOptionPane;
 
 public class menuppal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		cuentaBancaria edd = new cuentaBancaria();
-		cuentaBancaria maria = new cuentaBancaria();
-		cuentaBancaria elisa= new cuentaBancaria();
-		cuentaBancaria edgar = new cuentaBancaria();
-		edd.nombreCliente = "Edwin Ferney Barragan Muñoz";
-		System.out.println(edd.nombreCliente);
-		System.out.println();
-		
+		double resultado;
+		CuentaBancaria edd = new CuentaBancaria();
+		CuentaBancaria maria = new CuentaBancaria("maria fernanda ochoa","3012115588","Cra 1#1-1");
+		CuentaBancaria elisa= new CuentaBancaria();
+		CuentaBancaria edgar = new CuentaBancaria("edgar alonso mendez","3012115589","Cra 1#1-2",200000);
+		edd.setNombreCliente("Edwin Ferney Barragan Muñoz");
+		String mensaje = edd.retirar(1000000);
+		System.out.println(mensaje);			
+		edd.consignar(22000);
+		System.out.println(edd.getSaldo());		
+		System.out.println(edd.getDatosCliente());
+		System.out.println(edd.getFechaCreacion());
+		System.out.println();	
 		
 	}
 
