9e31dce67dfe34bcbbb347203ffd3d8a6759392b
dwn84 <50523550+dwn84@users.noreply.github.com>
2023-09-26 08:07:29 -0500

    Add files via upload

--- herencia/src/herencia/Persona.java
+++ herencia/src/herencia/Persona.java
@@ -1,22 +1,41 @@
 package herencia;
 
-public class Persona {
+public abstract class Persona {
 	private String cedula;
 	private String nombre;
 	private String telefono;
 	private String direccion;
 	
+	//constructores
+	public Persona() {
+		cedula = "999";
+		nombre = "N.N.";
+		telefono = "3012225588";
+		direccion = "Cra 1 #1-11";		
+	}	
+	
+	public Persona(String cedula, String nombre, String telefono, String direccion) {
+		super();
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
+	}
+
 	//polimorfismo por sobrecarga
 	public String saludar() {
 		return "Hola!, yo soy " + nombre + ", mucho gusto";
 	}
-	
+	//impedir que el método se pueda reescribir por los herederos
 	public String saludar(String nom, String dia) {
 		return "Hola!, yo soy " 
 				+ nombre + ", mucho gusto: " 
 				+ nom + " Hoy es:" + dia;
 	}
-	
+	//Métodos abstractos: sin código o implementación
+	public abstract void despedirse();
+	public abstract void transportarse();
+		
 	public String getCedula() {
 		return cedula;
 	}
@@ -40,6 +59,7 @@ public class Persona {
 	}
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
-	}
+	}	
+
 
 }
