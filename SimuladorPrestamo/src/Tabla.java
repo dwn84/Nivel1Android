9982d1f1299b9a1d17a2a0c988280bd49c798369
dwn84 <50523550+dwn84@users.noreply.github.com>
2023-10-18 10:06:01 -0500

    Add files via upload

--- SimuladorPrestamo/src/Tabla.java
+++ SimuladorPrestamo/src/Tabla.java
@@ -17,12 +17,15 @@ import java.awt.event.KeyEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Tabla extends JFrame {
 
+
+public class Tabla extends JFrame {
+	
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtValorPrestamo;
-	private JTextField txtMeses;
+	private JTextField txtMeses;	
+	
 
 	/**
 	 * Launch the application.
@@ -31,7 +34,7 @@ public class Tabla extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Tabla frame = new Tabla();
+					Tabla frame = new Tabla();					
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -44,6 +47,7 @@ public class Tabla extends JFrame {
 	 * Create the frame.
 	 */
 	public Tabla() {
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 566, 654);
 		contentPane = new JPanel();
@@ -76,28 +80,8 @@ public class Tabla extends JFrame {
 		txtValorPrestamo = new JTextField();
 		txtValorPrestamo.addKeyListener(new KeyAdapter() {			
 			@Override
-			public void keyTyped(KeyEvent e) {
-				char c = e.getKeyChar();				
-		        if (Character.isDigit(c)) {
-		        	String valorAnterior = txtValorPrestamo.getText();
-		        	String valorNuevo = valorAnterior + c;
-		        	double prestamo = Double.parseDouble(valorNuevo);
-		        	if(prestamo<1000000) {
-		        		lblAdvertenciaValor.setText("El mínimo es 1000000");
-		        		lblAdvertenciaValor.setForeground(Color.RED);
-		        	}else if(prestamo>500000000) {
-		        		lblAdvertenciaValor.setText("El máximo es 500000000");
-		        		lblAdvertenciaValor.setForeground(Color.RED);
-		        	}else {
-		        		lblAdvertenciaValor.setText("Monto Minimo:1.000.000- Monto Maximo:500.000.000 ");
-		        		lblAdvertenciaValor.setForeground(Color.BLACK);
-		        	}		        	
-		        	//JOptionPane.showMessageDialog(null, "Se ha presionado un numero");    
-		        }else{
-		        	e.consume();//Cancela el evento de recibir un dato
-		        	//JOptionPane.showMessageDialog(null, "Se ha ingresado una letra");
-		        }
-		        	
+			public void keyPressed(KeyEvent e) {				
+				validarTextField(e, lblAdvertenciaValor, txtValorPrestamo,1000000,500000000);
 			}
 		});
 
@@ -105,7 +89,18 @@ public class Tabla extends JFrame {
 		contentPane.add(txtValorPrestamo);
 		txtValorPrestamo.setColumns(10);
 		
+		JLabel lblElijeUnPlazo = new JLabel("Elije un plazo desde 4 y hasta 12 meses ");
+		lblElijeUnPlazo.setBounds(45, 211, 281, 14);
+		contentPane.add(lblElijeUnPlazo);
+		
 		txtMeses = new JTextField();
+		txtMeses.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				validarTextField(e, lblElijeUnPlazo, txtMeses,48,84);
+			}
+	
+		});
 		txtMeses.setBounds(164, 170, 86, 20);
 		contentPane.add(txtMeses);
 		txtMeses.setColumns(10);
@@ -114,15 +109,46 @@ public class Tabla extends JFrame {
 		lblNewLabel_2.setBounds(164, 236, 46, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		JButton btnCalcular = new JButton("Calcular");		
+		JButton btnCalcular = new JButton("Simular");		
 		btnCalcular.setEnabled(false);
 		btnCalcular.setBounds(338, 80, 162, 23);
 		contentPane.add(btnCalcular);
 		
-		JLabel lblElijeUnPlazo = new JLabel("Elije un plazo desde 4 y hasta 12 meses ");
-		lblElijeUnPlazo.setBounds(45, 211, 281, 14);
-		contentPane.add(lblElijeUnPlazo);
+		
 		
 		
 	}
+	private void validarTextField(KeyEvent e, JLabel etiqueta, JTextField cuadroTexto, int min, int max) {
+		char c = e.getKeyChar();				
+        if (Character.isDigit(c)) {
+        	String valorAnterior = cuadroTexto.getText();
+        	String valorNuevo = "";
+            if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+        	if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+        		
+        		JOptionPane.showMessageDialog(null, "Se ha presionado BORRAR");
+        		valorNuevo = valorAnterior.substring(0, valorAnterior.length() - 1);
+                
+        	}else{
+        		valorNuevo = valorAnterior + c;
+        	}
+        	
+        	
+        	double prestamo = Double.parseDouble(valorNuevo);
+        	etiqueta.setForeground(Color.RED);
+        	if(prestamo<min) {
+        		etiqueta.setText("El mínimo es " + min);        		
+        	}else if(prestamo>max) {
+        		etiqueta.setText("El máximo es " + max);        		
+        	}else {
+        		etiqueta.setText("Monto Minimo: " + min + " - Monto Maximo: " + max);
+        		etiqueta.setForeground(Color.BLACK);
+        	}		        	
+        	//JOptionPane.showMessageDialog(null, "Se ha presionado un numero");    
+        }else{		        	
+        	e.consume();//Cancela el evento de recibir un dato
+        	//JOptionPane.showMessageDialog(null, "Se ha ingresado una letra");
+        }
+		
+	} 
 }
